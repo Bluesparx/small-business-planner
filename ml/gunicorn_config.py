d47753b12ed0c3f5813d18860baf9102a51d09ca
Bluesparx <nazia.hassan2807@gmail.com>
2025-01-30 09:48:16 +0530

    gunicron

--- ml/gunicorn_config.py
+++ ml/gunicorn_config.py
@@ -1,4 +1,20 @@
-bind = "0.0.0.0:10000"
-workers = 2
+workers = 1
+worker_class = 'sync'
 threads = 1
 timeout = 600
+graceful_timeout = 120
+keep_alive = 5
+
+max_requests = 5
+max_requests_jitter = 2 
+
+limit_request_line = 4096
+limit_request_fields = 100
+limit_request_field_size = 8190
+
+capture_output = True
+enable_stdio_inheritance = True
+
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
